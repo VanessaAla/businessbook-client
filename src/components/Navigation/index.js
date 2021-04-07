7d1cb938335d374815010c72fb445bd9d61bb918
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-04-07 11:12:15 +0200

    created skeleton for login

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+
+export default function Navigation() {
+  return <div>Navigation</div>;
+}
---
+++ src/pages/Login/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import { Col } from "react-bootstrap";
+
+export default function Login() {
+  const [email, set_Email] = useState("");
+  const [password, set_Password] = useState("");
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    set_Email("");
+    set_Password("");
+  }
+
+  return (
+    <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <h1 className="mt-5 mb-5">Login</h1>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={(event) => set_Email(event.target.value)}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(event) => set_Password(event.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mt-5">
+          <Button variant="primary" type="submit" onClick={submitForm}>
+            Log in
+          </Button>
+        </Form.Group>
+        <Link to="/signup" style={{ textAlign: "center" }}>
+          Click here to sign up
+        </Link>
+      </Form>
+    </Container>
+  );
+}
